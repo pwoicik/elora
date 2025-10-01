3c3b6bb9a69a45a43e6d102ee3d3e7392aad7dcd
Patryk Wójcik <44197710+pwoicik@users.noreply.github.com>
2025-10-01 23:12:21 +0000

    hrm v1

---
+++ keyboards/splitkb/halcyon/elora/keymaps/custom/config.c
@@ -0,0 +1,2 @@
+#define TAPPING_TERM 200
+#define QUICK_TAP_TERM 0
--- keyboards/splitkb/halcyon/elora/keymaps/custom/keymap.c
+++ keyboards/splitkb/halcyon/elora/keymaps/custom/keymap.c
@@ -9,6 +9,15 @@ enum layers {
     MAGIC,
 };
 
+#define HOME_A  LCTL_T(KC_A)
+#define HOME_S  LSFT_T(KC_S)
+#define HOME_D  LGUI_T(KC_D)
+#define HOME_F  LALT_T(KC_F)
+#define HOME_J  LALT_T(KC_J)
+#define HOME_K  LGUI_T(KC_K)
+#define HOME_L  LSFT_T(KC_L)
+#define HOME_SCLN LCTL_T(KC_SCLN)
+
 #define SYM_ENT  LT(SYMBOL, KC_ENT)
 
 enum custom_keycodes {
@@ -83,7 +92,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [DEFAULT] = LAYOUT_elora_hlc(
      KC_EQL,       KC_1,         KC_2,         KC_3,         KC_4,         KC_5,                                                                   KC_6,         KC_7,         KC_8,         KC_9,         KC_0,         KC_MINS,
      KC_TAB,       KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,                                                                   KC_Y,         KC_U,         KC_I,         KC_O,         KC_P,         KC_BSLS,
-     KC_ESC,       KC_A,         KC_S,         KC_D,         KC_F,         KC_G,                                                                   KC_H,         KC_J,         KC_K,         KC_L,         KC_SCLN,      KC_QUOT,
+     KC_ESC,       HOME_A,       HOME_S,       HOME_D,       HOME_F,       KC_G,                                                                   KC_H,         HOME_J,       HOME_K,       HOME_L,       HOME_SCLN,    KC_QUOT,
      KC_GRV,       KC_Z,         KC_X,         KC_C,         KC_V,         KC_B,         CW_SFT,       KC_DEL,         HT_FUNCTION,  CW_SFT,       KC_N,         KC_M,         KC_COMM,      KC_DOT,       KC_SLSH,      HT_MAGIC,
                                                KC_NO,        KC_NO,        KC_NO,        KC_SPC,       KC_BSPC,        SYM_ENT,      LOWER_RALT,   KC_NO,        KC_NO,        KC_NO,
      KC_MUTE,      KC_NO,        KC_NO,        KC_NO,        KC_NO,                                                                                              KC_MUTE,      KC_NO,        KC_NO,        KC_NO,        KC_NO
