a7de76debe73102b91c44c865a74acf524a2d8a0
pwoicik <patrykwoicik@gmail.com>
2026-01-24 00:09:00 +0100

    new layout

--- keyboards/splitkb/halcyon/elora/keymaps/custom/keymap.c
+++ keyboards/splitkb/halcyon/elora/keymaps/custom/keymap.c
@@ -11,6 +11,7 @@
 #include "info_config.h"
 #include "keyboard.h"
 #include "keycodes.h"
+#include "keymap_us.h"
 #include "pointing_device.h"
 #include "process_tap_dance.h"
 #include "progmem.h"
@@ -26,11 +27,12 @@
 
 enum layers {
     DEFAULT = 0,
+    GALLIUM,
+
     LOWER,
     SYMBOL,
     FUNCTION,
     MAGIC,
-
     GAME,
     MOUSE,
 };
@@ -40,11 +42,11 @@ enum layers {
 #define HM_G(key) LGUI_T(key)
 #define HM_A(key) LALT_T(key)
 #define SEMI KC_SCLN
-#define TO_DEF TO(DEFAULT)
 #define MO_DEF MO(DEFAULT)
 
 enum custom_keycodes {
-    CW_SFT = SAFE_RANGE,
+    TO_DEF = SAFE_RANGE,
+    CW_SFT,
     HT_SYM,
     MO_SSLOW,
     MO_SMED,
@@ -53,7 +55,6 @@ enum custom_keycodes {
 };
 
 enum {
-    HSH_DEF,
     TD_MAGIC,
 };
 
@@ -71,7 +72,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [LOWER] = LAYOUT_elora_hlc(
      KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_HOME,                                                                KC_LPRN,      KC_NUM,       KC_PEQL,      KC_PSLS,      KC_PAST,      KC_NO,
      KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_END,                                                                 KC_RPRN,      KC_P7,        KC_P8,        KC_P9,        KC_PMNS,      KC_PSCR,
-     TO(DEFAULT),  KC_NO,        KC_LEFT,      KC_UP,        KC_RIGHT,     KC_PGUP,                                                                KC_PERC,      HM_A(KC_P4),  HM_G(KC_P5),  HM_S(KC_P6),  HM_C(KC_PPLS),KC_NO,
+     KC_NO,        KC_NO,        KC_LEFT,      KC_UP,        KC_RIGHT,     KC_PGUP,                                                                KC_PERC,      HM_A(KC_P4),  HM_G(KC_P5),  HM_S(KC_P6),  HM_C(KC_PPLS),KC_NO,
      KC_NO,        KC_NO,        KC_NO,        KC_DOWN,      KC_NO,        KC_PGDN,      KC_NO,        KC_DEL,         KC_NO,        KC_NO,        KC_COMM,      KC_P1,        KC_P2,        KC_P3,        KC_PENT,      TD(TD_MAGIC),
                                                KC_NO,        KC_NO,        KC_NO,        KC_SPC,       KC_BSPC,        KC_ENT,       KC_NO,        KC_NO,        KC_P0,        KC_PDOT,
      KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO,                                                                                              KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO
@@ -80,7 +81,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [SYMBOL] = LAYOUT_elora_hlc(
      KC_GRAVE,     KC_LBRC,      KC_LPRN,      KC_RPRN,      KC_RBRC,      KC_DOT,                                                                 KC_NO,        KC_NUM,       KC_PEQL,      KC_PSLS,      KC_PAST,      KC_NO,
      KC_EXLM,      KC_COMM,      KC_LCBR,      KC_RCBR,      KC_SCLN,      KC_QUES,                                                                KC_GRAVE,     KC_P7,        KC_P8,        KC_P9,        KC_PMNS,      KC_PSCR,
-     TD(HSH_DEF),  KC_CIRC,      KC_EQL,       KC_UNDS,      KC_DOLLAR,    KC_ASTR,                                                                KC_DQT,       KC_P4,        KC_P5,        KC_P6,        KC_PPLS,      KC_NO,
+     KC_HASH,      KC_CIRC,      KC_EQL,       KC_UNDS,      KC_DOLLAR,    KC_ASTR,                                                                KC_DQT,       KC_P4,        KC_P5,        KC_P6,        KC_PPLS,      KC_NO,
      KC_TILDE,     KC_LT,        KC_PIPE,      KC_MINS,      KC_GT,        KC_SLSH,      KC_BSLS,      KC_AT,          KC_NO,        KC_NO,        KC_QUOT,      KC_P1,        KC_P2,        KC_P3,        KC_PENT,      TD(TD_MAGIC),
                                                KC_AMPR,      KC_PLUS,      KC_NO,        KC_PERC,      KC_COLON,       KC_NO,        KC_NO,        KC_NO,        KC_P0,        KC_PDOT,
      KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO,                                                                                              KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO
@@ -96,9 +97,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [MAGIC] = LAYOUT_elora_hlc(
-     TO_DEF,       TO(LOWER),    TO(SYMBOL),   TO(GAME),     TO(FUNCTION), KC_NO,                                                                  KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO,
+     KC_NO,        KC_NO,        KC_NO,        TO(GAME),     KC_NO,        KC_NO,                                                                  KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO,
      KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO,                                                                  KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO,
-     KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO,                                                                  RM_TOGG,      RM_SATU,      RM_HUEU,      RM_VALU,      RM_NEXT,      KC_NO,
+     KC_NO,        DF(DEFAULT),  DF(GALLIUM),  KC_NO,        KC_NO,        KC_NO,                                                                  RM_TOGG,      RM_SATU,      RM_HUEU,      RM_VALU,      RM_NEXT,      KC_NO,
      KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO,          KC_NO,        KC_NO,        KC_NO,        RM_SATD,      RM_HUED,      RM_VALD,      RM_PREV,      TO_DEF,
                                                KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO,          KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO,
      KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO,                                                                                              KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO
@@ -106,7 +107,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [GAME] = LAYOUT_elora_hlc(
      KC_ESC,       KC_I,         KC_2,         KC_3,         KC_4,         KC_5,                                                                   KC_F1,        KC_F2,        KC_F3,        KC_F4,        KC_F5,        KC_F6,
-     LGUI(KC_R),   KC_TAB,       KC_Q,         KC_1,         KC_E,         KC_R,                                                                   KC_6,         KC_7,         KC_8,         KC_9,         KC_0,         KC_NO,
+     KC_F13,       KC_TAB,       KC_Q,         KC_1,         KC_E,         KC_R,                                                                   KC_6,         KC_7,         KC_8,         KC_9,         KC_0,         KC_NO,
      KC_G,         KC_LSFT,      KC_A,         KC_W,         KC_D,         KC_F,                                                                   KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO,
      KC_B,         KC_LCTL,      KC_Z,         KC_S,         KC_C,         KC_V,         KC_LALT,      KC_ENT,         MO_DEF,       MO_DEF,       KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO,        TO_DEF,
                                                KC_M,         KC_T,         KC_X,         KC_SPC,       KC_NO,          MO_DEF,       MO_DEF,       KC_NO,        KC_NO,        KC_NO,
@@ -119,6 +120,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_ESC,       KC_LCTL,      KC_LSFT,      KC_LGUI,      KC_LALT,      KC_NO,                                                                  KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO,
      KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO,          KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO,
                                                KC_NO,        KC_NO,        MS_BTN2,      MS_BTN1,      MS_BTN3,        KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO,
+     KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO,                                                                                              KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO
+    ),
+
+    [GALLIUM] = LAYOUT_elora_hlc(
+     KC_EQL,       KC_1,         KC_2,         KC_3,         KC_4,         KC_5,                                                                   KC_6,         KC_7,         KC_8,         KC_9,         KC_0,         KC_MINS,
+     KC_TAB,       KC_B,         KC_L,         KC_D,         KC_C,         KC_V,                                                                   KC_J,         KC_F,         KC_O,         KC_U,         KC_COMMA,     KC_BSLS,
+     KC_ESC,       HM_C(KC_N),   HM_S(KC_R),   HM_G(KC_T),   HM_A(KC_S),   KC_G,                                                                   KC_Y,         HM_A(KC_H),   HM_G(KC_A),   HM_S(KC_E),   HM_C(KC_I),   KC_SLASH,
+     KC_GRV,       KC_X,         KC_Q,         KC_M,         KC_W,         KC_Z,         CW_SFT,       KC_DEL,         MO(FUNCTION), CW_SFT,       KC_K,         KC_P,         KC_QUOT,      SEMI,         KC_DOT,       TD(TD_MAGIC),
+                                               KC_NO,        MO(FUNCTION), MO(LOWER),    KC_SPC,       KC_BSPC,        HT_SYM ,      KC_RALT,      MO(LOWER),    MO(FUNCTION), KC_NO,
      KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO,                                                                                              KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO
     ),
 };
@@ -176,6 +186,12 @@ bool htkl_interrupted = false;
 bool process_record_user(uint16_t keycode, keyrecord_t* record) {
     // NOLINTNEXTLINE(bugprone-switch-missing-default-case)
     switch (keycode) {
+        case TO_DEF:
+            if (record->event.pressed) {
+                layer_clear();
+            }
+            return false;
+
         case CW_SFT:
             return hold_tap_key_func(KC_LSFT, caps_word_toggle, record);
 
@@ -331,7 +347,6 @@ void magic_dance_reset(tap_dance_state_t* state, void* user_data) {
 }
 
 tap_dance_action_t tap_dance_actions[] = {
-    [HSH_DEF]  = ACTION_TAP_DANCE_LAYER_MOVE(KC_HASH, DEFAULT),
     [TD_MAGIC] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, magic_dance_finished, magic_dance_reset),
 };
 
@@ -356,7 +371,11 @@ void raw_hid_receive(uint8_t* data, uint8_t length) {
     switch (type) {
         case COMMAND_TYPE_CHANGE_LAYER: {
             uint8_t layer = data[1];
-            layer_move(layer);
+            if (layer == 0) {
+                layer_clear();
+            } else {
+                layer_move(layer);
+            }
         } break;
     }
 }
