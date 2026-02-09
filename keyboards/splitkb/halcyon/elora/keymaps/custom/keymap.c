1c03a15b186baa923572cf3804667fe9acacb0b9
pwoicik <patrykwoicik@gmail.com>
2026-02-09 22:17:03 +0100

    adjustments

--- keyboards/splitkb/halcyon/elora/keymaps/custom/keymap.c
+++ keyboards/splitkb/halcyon/elora/keymaps/custom/keymap.c
@@ -64,9 +64,9 @@ enum {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [DEFAULT] = LAYOUT_elora_hlc(
      KC_EQL,       KC_1,         KC_2,         KC_3,         KC_4,         KC_5,                                                                   KC_6,         KC_7,         KC_8,         KC_9,         KC_0,         KC_MINS,
-     KC_TAB,       KC_B,         KC_L,         KC_D,         KC_C,         KC_V,                                                                   KC_J,         KC_F,         KC_O,         KC_U,         KC_COMMA,     KC_BSLS,
-     KC_ESC,       HM_C(KC_N),   HM_S(KC_Z),   HM_G(KC_T),   HM_A(KC_S),   KC_G,                                                                   KC_Y,         HM_A(KC_H),   HM_G(KC_A),   HM_S(KC_E),   HM_C(KC_I),   KC_SLASH,
-     KC_GRV,       KC_X,         KC_Q,         KC_M,         KC_W,         KC_NO,        CW_SFT,       KC_DEL,         MO(FUNCTION), CW_SFT,       KC_K,         KC_P,         KC_QUOT,      SEMI,         KC_DOT,       TD(TD_MAGIC),
+     KC_TAB,       KC_B,         KC_L,         KC_D,         KC_C,         KC_V,                                                                   KC_J,         KC_F,         KC_O,         KC_U,         SEMI,         KC_BSLS,
+     KC_ESC,       HM_C(KC_N),   HM_S(KC_Z),   HM_G(KC_T),   HM_A(KC_S),   KC_G,                                                                   KC_Y,         HM_A(KC_H),   HM_G(KC_A),   HM_S(KC_E),   HM_C(KC_I),   KC_QUOT,
+     KC_GRV,       KC_X,         KC_Q,         KC_M,         KC_W,         KC_NO,        CW_SFT,       KC_DEL,         MO(FUNCTION), CW_SFT,       KC_K,         KC_P,         KC_COMM,      KC_DOT,       KC_SLSH,      TD(TD_MAGIC),
                                                KC_NO,        MO(FUNCTION), MO(LOWER),    KC_SPC,       KC_BSPC,        HT_SYM ,      MT_ALT_R,     MO(LOWER),    MO(FUNCTION), KC_NO,
      KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO,                                                                                              KC_NO,        KC_NO,        KC_NO,        KC_NO,        KC_NO
     ),
